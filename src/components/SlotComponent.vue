44d7462fd000d051a37f73a2a45babb3688bc3de
melisasahira <melisasahira@student.uir.ac.id>
2024-05-27 14:49:07 +0700

    first commit

---
+++ src/components/SlotComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Slot Component</h2>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SlotComponent'
+}
+</script>
+
+<style scoped>
+h2 {
+  color: #eff0ef;
+}
+</style>
